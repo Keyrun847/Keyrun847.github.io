6a303952ae053fd103b6e49348843f6da5a31ca0
Keyrun847 <66483399+Keyrun847@users.noreply.github.com>
2021-05-15 19:19:38 +0530

    g

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,13 @@ function closeNav() {
   document.getElementById("open").style.display = "block";
   document.getElementById("close").style.display = "none";
 }
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
